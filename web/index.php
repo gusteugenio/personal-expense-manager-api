ce2612430d88a94bc931c0cabef19f4a9a2f7df0
gusteugenio <gustavoeugenio297@gmail.com>
2026-07-17 03:55:38 -0300

    fix: torna carregamento do .env opcional nos pontos de entrada - troca dotenv->load() por safeLoad() no yii (console) e web/index.php - evita erro fatal quando variaveis vem do ambiente (railway) sem arquivo .env

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
+$dotenv->safeLoad();
 
 $config = require __DIR__ . '/../config/web.php';
 
